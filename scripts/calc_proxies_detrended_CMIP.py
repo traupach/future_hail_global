Updates and bug fix in detrended proxy calculation.

scripts/calc_proxies_detrended_CMIP.py
@@ -1,12 +1,10 @@
 import os
 import sys
-import json
 import xarray
-import numpy as np
-import pandas as pd
 from glob import glob
 import modules.fut_hail as fh
 import modules.updated_proxy as up
+from dask.distributed import Client
 
 base_dir = '/g/data/up6/tr2908/future_hail_global/CMIP_conv/' # Base directory under which to find CMIP6 files.
 out_dir = '/g/data/up6/tr2908/future_hail_global/CMIP_detrended/proxy_results' # Output directory for cached outputs.
@@ -21,6 +19,11 @@ detrended_ing = ['freezing_level', 'lapse_rate_700_500', 'melting_level', 'mixed
 assert len(sys.argv) == 2, 'Usage: calc_proxies.py <model_number>'
 model_num = int(sys.argv[1])
 
+# Start a dask cluster.
+print('Starting cluster...')
+client = Client(scheduler_file='sched_' + os.environ['PBS_JOBID'] + '.json')
+print(client, flush=True)
+
 # Get model directory.
 model_dirs = sorted(glob(f'{base_dir}/*/'))
 model_dir = model_dirs[model_num]
@@ -34,7 +37,7 @@ print(outfile)
 
 # Open input data.
 dat = xarray.open_mfdataset(f'{subdir}/*.nc', parallel=True)
-dat = dat.chunk({'time': 1000, 'lat': -1, 'lon': -1})
+dat = dat.chunk({'time': 3000, 'lat': -1, 'lon': -1})
 
 for ing in detrended_ing:
     outfile_ing = f'{outfile}{ing}.nc'
@@ -47,37 +50,31 @@ for ing in detrended_ing:
     
         # Open detrended data.
         detrended_ingredient = xarray.open_dataset(detrended_file)[ing]
-        detrended_ingredient = detrended_ingredient.chunk({'time': 1000, 'lat': -1, 'lon': -1})
+        detrended_ingredient = detrended_ingredient.load().chunk({'time': 3000, 'lat': -1, 'lon': -1})
     
         # Align times.
-        if not np.all(dat.time == detrended_ingredient.time):
-            dat = dat.sel(time=detrended_ingredient.time)
-        assert np.all(dat.time == detrended_ingredient.time), 'Time mismatch.'
-    
+        dat = dat.sel(time=detrended_ingredient.time)
+        assert dat.time.equals(detrended_ingredient.time), 'Time mismatch.'
+        
         # Replace detrended ingredient.
         detrended = dat.copy()
         detrended[ing] = detrended_ingredient
     
         # Calculate literature cape-shear proxies.
         print('Literature proxies...')
-        prox = fh.storm_proxies(dat=dat)
-    
+        prox = fh.storm_proxies(dat=detrended, proxies={'proxy_Eccel2012': 'Eccel 2012',
+                                                  'proxy_SHIP_0.1': 'SHIP > 0.1'})
+
         # Add Raupach hail proxies.
         print('Raupach proxies...')
-        prox['proxy_Raupach2023_original'] = up.apply_Raupach_proxy(dat=dat, results_file=proxy_results_file, 
-                                                                    extra_conds_file=proxy_conds_file, load=False, 
-                                                                    band_limits=None)
-        prox['proxy_Raupach2023_original_noconds'] = up.apply_Raupach_proxy(dat=dat, results_file=proxy_results_file, 
-                                                                            extra_conds_file=None, load=False,
-                                                                            band_limits=None)
-        prox['proxy_Raupach2023_updated'] = up.apply_Raupach_proxy(dat=dat, results_file=proxy_results_file, 
+        prox['proxy_Raupach2023_updated'] = up.apply_Raupach_proxy(dat=detrended, results_file=proxy_results_file, 
                                                                    extra_conds_file=proxy_conds_file,
                                                                    band_limits=[2000, None], load=False)
-        prox['proxy_Raupach2023_updated_noconds'] = up.apply_Raupach_proxy(dat=dat, results_file=proxy_results_file, 
+        prox['proxy_Raupach2023_updated_noconds'] = up.apply_Raupach_proxy(dat=detrended, results_file=proxy_results_file, 
                                                                            extra_conds_file=None,
                                                                            band_limits=[2000, None], load=False)
     
         prox.attrs['note'] = f'Proxies calculated for 3C epoch with ingredient {ing} detrended to match historical period.'
         fh.write_output(dat=prox, file=outfile_ing)
     
-        del detrended, detrended_ing, prox
+        del detrended, detrended_ingredient, prox